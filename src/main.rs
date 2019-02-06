8883f60eb496f6dfd856732e014c2bc1e5465f9e
emersion <contact@emersion.fr>
2019-02-06 16:18:38 +0100

    Add a very basic Rust code formatter

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod ast;
 use pest::Parser;
 use std::collections::HashMap;
 use crate::ast::*;
+use std::io::{Write, Result, stdout};
 
 #[derive(Parser)]
 #[grammar = "lustre.pest"]
@@ -126,5 +127,96 @@ fn main() {
 		pair.into_inner().map(parse_node).collect()
 	}
 
-	println!("{:?}", parse_file(successful_parse.unwrap().next().unwrap()));
+	let f = parse_file(successful_parse.unwrap().next().unwrap());
+	println!("{:?}", &f);
+
+	fn format_const(w: &mut Write, c: &Const) -> Result<()> {
+		match c {
+			Const::Unit => write!(w, "()"),
+			Const::Bool(b) => write!(w, "{}", b),
+			Const::Int(i) => write!(w, "{}", i),
+			Const::Float(f) => write!(w, "{}", f),
+			Const::String(s) => write!(w, "\"{}\"", s), // TODO: escaping
+		}
+	}
+
+	fn format_expr(w: &mut Write, e: &Expr) -> Result<()> {
+		match e {
+			Expr::Call{name, args} => {
+				write!(w, "{}(", name)?;
+				let mut first = true;
+				for arg in args {
+					format_expr(w, arg)?;
+					if !first {
+						write!(w, ", ")?;
+					}
+					first = false;
+				}
+				write!(w, ")")
+			},
+			Expr::Const(c) => format_const(w, c),
+		}
+	}
+
+	fn format_equation(w: &mut Write, eq: &Equation) -> Result<()> {
+		write!(w, "\tlet {} = ", &eq.name)?;
+		format_expr(w, &eq.value)?;
+		write!(w, ";\n")
+	}
+
+	fn format_arg_list(w: &mut Write, args: &HashMap<String, Type>, with_name: bool, with_typ: bool) -> Result<()> {
+		let mut first = true;
+		for (name, typ) in args {
+			if with_name {
+				write!(w, "{}", name)?;
+			}
+			if with_name && with_typ {
+				write!(w, ": ")?;
+			}
+			if with_typ {
+				write!(w, "{}", match typ {
+					Type::Unit => "()",
+					Type::Bool => "bool",
+					Type::Int => "i32",
+					Type::Float => "f32",
+					Type::String => "String",
+				})?;
+			}
+			if !first {
+				write!(w, ", ")?;
+			}
+			first = false;
+		}
+		Ok(())
+	}
+
+	fn format_node(w: &mut Write, n: &Node) -> Result<()> {
+		write!(w, "fn {}(", &n.name)?;
+		format_arg_list(w, &n.args_in, true, true)?;
+		write!(w, ") -> (")?;
+		format_arg_list(w, &n.args_out, false, true)?;
+		write!(w, ") {{\n")?;
+		for eq in &n.body {
+			format_equation(w, &eq)?;
+		}
+		write!(w, "\treturn (")?;
+		format_arg_list(w, &n.args_out, true, false)?;
+		write!(w, ");\n")?;
+		write!(w, "}}\n\n")
+	}
+
+	fn format_file(w: &mut Write, f: &[Node]) -> Result<()> {
+		write!(w, "fn print(s: &str) {{\n")?;
+		write!(w, "\tprintln!(\"{{}}\", s);\n")?;
+		write!(w, "}}\n\n")?;
+
+		for n in f {
+			format_node(w, &n)?;
+		}
+
+		write!(w, "fn main() {{}}\n")?;
+		Ok(())
+	}
+
+	format_file(&mut stdout(), &f).unwrap();
 }
